6d243e4354d3dffeb7d9c5eaa13521c648e858a9
KevinAlava <107556731+KevinAlava@users.noreply.github.com>
2022-06-23 20:17:25 -0500

    Segunda Interaccion
    Continuación del código

--- 3. Code/inicioSesionUser.py
+++ 3. Code/inicioSesionUser.py
@@ -4,3 +4,9 @@ print ("1. Iniciar sesion")
 print ("2. Registrarse")
 opcion = input()
 if opcion == "1":
+ print ("Por favor ingrese su nombre de usuario:")
+    usuario = input()
+    print ("Por favor ingrese su contraseña:")
+    contraseña = input()
+    if usuario == "admin" and contraseña == "255075":
+        print ("Bienvenido administrador")
