55cdbc57947abe631aba1844653729bafc4bafde
Jhois Cedeño <105936031+micaelCZ@users.noreply.github.com>
2022-06-23 20:11:20 -0500

    Primera Interacción
    Primeras líneas del código.

---
+++ 3. Code/inicioSesionUser.py
@@ -0,0 +1,6 @@
+print ("Bienvenido al sistema de venta/compra de productos tecnologicos")
+print ("Desea iniciar sesion o registrarse")
+print ("1. Iniciar sesion")
+print ("2. Registrarse")
+opcion = input()
+if opcion == "1":
