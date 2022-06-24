85c7d9e8b711a8f0f0bdd254104db285aa6f45b4
Wagner Cuenca <107556443+Wgnrcc@users.noreply.github.com>
2022-06-23 20:18:46 -0500

    interacción 3
    continuación del código

--- 3. Code/inicioSesionUser.py
+++ 3. Code/inicioSesionUser.py
@@ -10,3 +10,13 @@ if opcion == "1":
     contraseña = input()
     if usuario == "admin" and contraseña == "255075":
         print ("Bienvenido administrador")
+ if opcion == "2":
+    print ("Por favor ingrese su nombre de usuario:")
+    usuario = input()
+    print ("Por favor ingrese su contraseña:")
+    contraseña = input()
+    #crea un archivo de texto con el nombre de usuario y contraseña ingresados
+    archivo = open("usuarios.txt", "a")
+    archivo.write(usuario + " " + contraseña + "\n")
+    archivo.close()
+    print ("Usuario registrado")
