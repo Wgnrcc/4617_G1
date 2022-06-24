09b5c003059431f3490eaa406737758e59461b23
Jhois Cedeño <105936031+micaelCZ@users.noreply.github.com>
2022-06-23 20:22:04 -0500

    Primera Interacción
    Primeras líneas de código

---
+++ 3. Code/inicioSesionAdmin.py
@@ -0,0 +1,7 @@
+print("Bienvenido al sistema de administración")
+print("Por favor ingrese su nombre de usuario:")
+usuario = input()
+print("Por favor ingrese su contraseña:")
+contraseña = input()
+if usuario == "admin" and contraseña == "255075":
+    print("Bienvenido administrador")
